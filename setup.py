f2203d4fe5c1d6a5c1467544a08f63e9e60b5141
saisharathchandramahankali <saisarathchandra.mahankali@gwu.edu>
2025-10-06 15:10:47 -0500

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Generative AI Project',
+    version='0.0.0',
+    author='Sai Sharath Chandra Mahankali',
+    packages=find_packages(),
+    author_email='msschandra2002@gmail.com',
+    install_requires=[]
+)
